0eea114335b5d069f44a9005c83cef87020a1ab9
jash200115 <jashroy29@gmail.com>
2026-01-19 02:06:35 +0530

    Initial commit: Spring Boot Quiz App

---
+++ src/main/java/com/telusko/quizapp/service/QuestionService.java
@@ -0,0 +1,19 @@
+package com.telusko.quizapp.service;
+
+import com.telusko.quizapp.Dao.QuestionDao;
+import com.telusko.quizapp.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    QuestionDao questionDao;
+
+    public List<Question> getAllQuestions() {
+        return questionDao.findAll();
+    }
+}
