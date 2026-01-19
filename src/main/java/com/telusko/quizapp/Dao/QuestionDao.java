958e30a9623a3f0bbd717fb2f490f0bf1c42b7ef
jash200115 <jashroy29@gmail.com>
2026-01-19 16:37:16 +0530

    Creating quiz

--- src/main/java/com/telusko/quizapp/Dao/QuestionDao.java
+++ src/main/java/com/telusko/quizapp/Dao/QuestionDao.java
@@ -2,6 +2,7 @@ package com.telusko.quizapp.Dao;
 
 import com.telusko.quizapp.Model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +11,7 @@ import java.util.List;
 public interface QuestionDao extends JpaRepository<Question, Integer> {
 
     List<Question> findByCategory(String category);
+
+    @Query(value = "SELECT * FROM question q Where q.category=:category ORDER_BY_RANDOM() LIMIT :numQ", nativeQuery = true)
+    List<Question> findRandomQuestionByCategory(String category, int numQ);
 }
